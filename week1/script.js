3df262fafa063da34382cec86b2a4dbe13b154b8
AlexBuck1 <alexcbuck1@gmail.com>
2023-01-07 13:52:31 -0800

    week1

---
+++ week1/script.js
@@ -0,0 +1,6 @@
+let firstName = "Alex";
+let lastName = "Buck";
+let yearsOfStudy = 1.5;
+let goal = "get an internship";
+
+document.getElementById("demo").innerHTML = 'My name is ' + firstName + ' ' + lastName + '. I have been at SCC for ' + yearsOfStudy + ' years and plan to ' + goal + ' after completing my courses.';
